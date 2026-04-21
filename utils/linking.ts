c426eda2af415642f876a07e82e7c1abdba29fd6
Theo Dumontet <theo.dumontet@egs.school>
2026-04-21 16:12:23 +0200

    products, modals

---
+++ utils/linking.ts
@@ -0,0 +1,33 @@
+import * as Linking from 'expo-linking';
+
+const prefix = Linking.createURL('/');
+
+export const linking = {
+  prefixes: [prefix, 'app://'],
+  
+  config: {
+    screens: {
+      // Auth screens
+      '(auth)': {
+        screens: {
+          login: 'login',
+          signup: 'signup',
+        },
+      },
+      
+      // App screens
+      '(tabs)': {
+        screens: {
+          index: 'home',
+          favorites: 'favorites',
+          'product/[id]': 'product/:id',
+          checkout: 'checkout',
+          'order-confirmation': 'order-confirmation',
+        },
+      },
+      
+      // Catch all
+      '*': '*',
+    },
+  },
+};
